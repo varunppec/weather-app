8ddcdfe38b3c4b6a1110f0b37b96039dd0f1b94f
Varun B D <varun.ppec@gmail.com>
2021-10-06 03:01:08 +0530

    initial commit

---
+++ src/getdata.js
@@ -0,0 +1,51 @@
+const getData = async function (name, e) {
+  try {
+    const response = await fetch(
+      "http://api.openweathermap.org/data/2.5/weather?q=" +
+        name +
+        "&APPID=c6d173aa27f915a5ce2336f7f0768442",
+      { mode: "cors" }
+    );
+    const data = await response.json();
+    console.log(data);
+    if (data.cod > 300) {
+      throw "error";
+    }
+    localStorage.setItem("name", data.name);
+    if (e == "celsius")
+      return {
+        description: data.weather[0].description,
+        name: data.name,
+        country: data.sys.country,
+        temp: convertTempToC(data.main.temp) + " \xB0C",
+        feelslike: convertTempToC(data.main.feels_like) + " \xB0C",
+        humidity: data.main.humidity,
+        wind: data.wind.speed,
+        id: data.weather[0].icon
+      };
+    if (e == "fahr")
+      return {
+        description: data.weather[0].description,
+        name: data.name,
+        country: data.sys.country,
+        temp: convertTempToF(convertTempToC(data.main.temp)) + " \xB0F",
+        feelslike:
+          convertTempToF(convertTempToC(data.main.feels_like)) + " \xB0F",
+        humidity: data.main.humidity,
+        wind: data.wind.speed,
+        id: data.weather[0].icon
+      };
+  } catch {
+    window.alert("error");
+    return "error";
+  }
+};
+
+const convertTempToC = function (temp) {
+  return Math.round((temp - 273.15) * 10) / 10;
+};
+
+const convertTempToF = function (temp) {
+  return Math.round((temp * 1.8 + 32) * 10) / 10;
+};
+export { getData };
---
+++ src/index.js
@@ -0,0 +1,107 @@
+import { getData } from "./getdata";
+
+const pageLoad = async function (initial) {
+  const header = document.querySelector("#header");
+  const heading = document.createElement("div");
+  heading.id = "heading";
+  heading.innerText = "Weather App";
+  const input = document.createElement("input");
+  input.id = "input";
+  input.addEventListener("keydown", function (e) {
+    onEnter(e, input.value);
+  });
+
+  const button = document.createElement("button");
+  button.id = "toggle";
+  //button.innerText = '\xB0C | \xB0F';
+  const celsius = document.createElement("span");
+  const fahr = document.createElement("span");
+  celsius.id = "celsius";
+  celsius.classList.add("toggled");
+  fahr.id = "fahr";
+  celsius.innerText = "\xB0C";
+  fahr.innerText = "\xB0F";
+  button.onclick = async function () {
+    celsius.classList.toggle("toggled");
+    fahr.classList.toggle("toggled");
+    clearData();
+    const toggle = document.querySelector(".toggled").id;
+    let data = await getData(localStorage.getItem("name"), toggle);
+    if (data != "error" && data) {
+      fillData(data);
+    }
+  };
+  const test = await getData(initial, "celsius");
+  fillData(test);
+  button.prepend(celsius, fahr);
+  header.append(heading, input, button);
+};
+
+const onEnter = async function (e, name) {
+  if (!name) return;
+  if (e.key == "Enter") {
+    const toggle = document.querySelector(".toggled").id;
+    let data = await getData(name, toggle);
+    if (data != "error" && data) {
+      clearData();
+      fillData(data);
+    }
+  }
+};
+const fillData = function (data) {
+  const container = document.querySelector("#container");
+  const div = document.createElement("div");
+  div.id = "info";
+  /*
+  Object.values(data).forEach(function (x) {
+    const field = document.createElement("div");
+    field.innerText = x;
+    div.append(field);
+  });
+  */
+  const general = document.createElement("div");
+  general.id = "general";
+  const description = document.createElement("div");
+  description.id = "description";
+  description.innerText = data.description;
+  const location = document.createElement("div");
+  location.id = "location";
+  location.innerText = data.name + ", " + data.country;
+  general.append(description, location);
+
+  const advanced = document.createElement("div");
+  advanced.id = "advanced";
+  const temp = document.createElement("div");
+  temp.id = "temp";
+  temp.innerText = data.temp;
+  const details = document.createElement("div");
+  details.id = "details";
+  const feels = document.createElement("div");
+  feels.innerText = "Feels like: " + data.feelslike;
+  const humidity = document.createElement("div");
+  humidity.innerText = "Humidity: " + data.humidity;
+  const wind = document.createElement("div");
+  wind.innerText = "Wind: " + data.wind;
+
+  const img = document.createElement("img");
+  img.src = "http://openweathermap.org/img/wn/" + data.id + "@2x.png";
+  img.id = "image";
+  description.append(img);
+
+  details.append(feels, humidity, wind);
+  advanced.append(temp, details);
+
+  div.append(general, advanced);
+  div.style.opacity = '0';
+  setTimeout(function() {
+    div.style.opacity = '1';
+  }, 0);
+  container.append(div);
+};
+
+const clearData = function () {
+  const info = document.querySelector("#info");
+  if (info) info.parentElement.removeChild(info);
+};
+
+pageLoad("London");
